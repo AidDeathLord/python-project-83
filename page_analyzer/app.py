d4af65aea33076d6699615228153716be3716f2b
Ihar <kostyuchik22@gmail.com>
2024-06-19 09:21:30 +0300

    add index.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,14 @@
 from dotenv import load_dotenv
 from flask import Flask, render_template
+import psycopg2
 import os
 
 
 load_dotenv()
+
+# DATABASE_URL = os.getenv('DATABASE_URL')
+# conn = psycopg2.connect(DATABASE_URL)
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
